b32598a5d369447367c381b899eac163800b1161
harshmarolia <53485461+HarshMarolia@users.noreply.github.com>
2021-02-09 22:03:38 +0530

    Fixed some code, and applied animation to drums.

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i = 0; i < numberOfDrumButtons; i++) {
 
-  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
     var buttonInnerHTML = this.innerHTML;
 
     makeSound(buttonInnerHTML);
 
-   
+    buttonAnimation(buttonInnerHTML);
 
   });
 
 }
 
-document.addEventListener("keypress", function(event) {
+document.addEventListener("keypress", function (event) {
 
   makeSound(event.key);
 
@@ -67,5 +67,12 @@ function makeSound(key) {
   }
 }
 
+function buttonAnimation(currentKey) {
 
+  var activeButton = document.querySelector("." + currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function () {
+    activeButton.classList.remove("pressed");
+  }, 100);
+}
 
